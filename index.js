9300f0095688968ddaa86fb45c96d76533021a28
andydangVN <dangminhnghia@gmail.com>
2022-05-24 15:12:41 +0700

    Manipulate text content attribute

---
+++ index.js
@@ -0,0 +1,11 @@
+// css
+$("h1").css("color","green")
+
+// Hmtl text - content manipulate
+$("h1").html("Goodbye")
+$("button").text("JQuery")
+
+// Manipulate element attribute
+$("a").attr("href","https://www.bing.com")
+
+
